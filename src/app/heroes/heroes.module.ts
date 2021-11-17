caa27bb50451b46f8a53721b11fa799bcb0f1e86
David Castrillón <david.castri@hotmail.es>
2021-11-17 15:11:57 +0100

    Importo Flex Layout Module en el módulo de heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -20,7 +23,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class HeroesModule { }
