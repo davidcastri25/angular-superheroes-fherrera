a1dad18459bcc2e29736ff320c4d871da57bed0d
David Castrillón <david.castri@hotmail.es>
2021-11-24 16:40:47 +0100

    Buscador con sugerencias

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -24,4 +24,9 @@ export class HeroesService {
   getHeroePorId(id: string): Observable<Heroe> {
     return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
+
+  //GET: 
+  getSugerencias (termino: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`);
+  }
 }
